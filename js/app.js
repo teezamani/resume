4a260e67a7ef726f1c21e4c86c1813e8c0b414f5
teezamani <tee4tayour@gmail.com>
2020-06-27 23:13:45 +0100

    remove typewriter

--- js/app.js
+++ js/app.js
@@ -14,29 +14,29 @@
         }
 
     // TYPEWRITER
-    let typeString = [
-      "I Live",
-      "I learn",
-      "To Relearn",
-      "What I learn't",
-      "And I Love to Code",
-      "I am theBeardedDeveloper ",
-      "Teezamani"
-  ];
-  let i = 0;
-  let count = 0;
-  let selectedText = "";
-  let text = "";
-  (type = () => {
-      if (count == typeString.length) {
-          count = 0;
-      }
-      selectedText = typeString[count];
-      text = selectedText.slice(0, ++i);
-      document.getElementById("typing").innerHTML = text;
-      if (text.length === selectedText.length) {
-          count++;
-          i = 0;
-      }
-      setTimeout(type, 250);
-  })();
+//     let typeString = [
+//       "I Live",
+//       "I learn",
+//       "To Relearn",
+//       "What I learn't",
+//       "And I Love to Code",
+//       "I am theBeardedDeveloper ",
+//       "Teezamani"
+//   ];
+//   let i = 0;
+//   let count = 0;
+//   let selectedText = "";
+//   let text = "";
+//   (type = () => {
+//       if (count == typeString.length) {
+//           count = 0;
+//       }
+//       selectedText = typeString[count];
+//       text = selectedText.slice(0, ++i);
+//       document.getElementById("typing").innerHTML = text;
+//       if (text.length === selectedText.length) {
+//           count++;
+//           i = 0;
+//       }
+//       setTimeout(type, 250);
+//   })();
